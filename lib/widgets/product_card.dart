ef5d58386d52fbbcbcd349dd448348705b8d65ee
Franklin <201701718g@utea.edu.pe>
2021-08-09 17:10:00 -0500

    6. Widget-ProductCard-Parte2

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -11,60 +11,128 @@ class ProductCard extends StatelessWidget {
         margin: EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        decoration: _cardBorders(),  
+        decoration: _cardBorders(),
         child: Stack(
           alignment: Alignment.bottomLeft,
           children: [
-
             _BackgroundImage(),
-            
-            _ProductDetails()
-
+            _ProductDetails(),
+            Positioned(top: 0, right: 0, child: _PriceTag()),
+            Positioned(top: 0, left: 0, child: _NotAvailable())
           ],
-        ),     
+        ),
       ),
     );
   }
 
   BoxDecoration _cardBorders() => BoxDecoration(
-     color: Colors.white,
-     borderRadius: BorderRadius.circular(25),
-     boxShadow: [
-       BoxShadow(
-         color: Colors.black12,
-         offset: Offset(0,7),
-         blurRadius: 10
-       )
-     ]
-  );
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, offset: Offset(0, 7), blurRadius: 10)
+          ]);
 }
 
-class _ProductDetails extends StatelessWidget {
-  
-
+class _NotAvailable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
       height: 70,
-      color: Colors.indigo,
+      decoration: BoxDecoration(
+          color: Colors.yellow[800],
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
     );
   }
 }
 
-class _BackgroundImage extends StatelessWidget {
- 
+class _PriceTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            '\$103.99',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+    );
+  }
+}
 
+class _ProductDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 50),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        //color: Colors.indigo,
+        decoration: _buildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Disco Duro',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'Id del disco Duro',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.indigo,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
+}
+
+class _BackgroundImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(25),
       child: Container(
-        width:  double.infinity,
+        width: double.infinity,
         height: 400,
         child: FadeInImage(
           placeholder: AssetImage('assets/jar-loading.gif'),
-          image:  NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
           fit: BoxFit.cover,
         ),
       ),
