27dd9a019a06ce10b5fee0ca791ef6d96064af60
Franklin <201701718g@utea.edu.pe>
2021-08-09 10:18:45 -0500

    3.Pantalla de productos - HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
+
 
 
 class HomeScreen extends StatelessWidget {
@@ -6,9 +8,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-     ),
+      appBar: AppBar(
+        title: Text("Productos"),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext, int index)=>ProductCard(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+
+        },
+      ),
    );
   }
 }
