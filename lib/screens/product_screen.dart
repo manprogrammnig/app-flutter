b4c098bff5fa74d920c251cd609e7be356aa5f52
Franklin <201701718g@utea.edu.pe>
2021-08-15 14:39:54 -0500

    17. Solo numeros y cambiar el Swich de disponibilidad

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:productos_app/providers/product_form_provider.dart';
 import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
@@ -8,6 +10,22 @@ class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productService = Provider.of<ProductsService>(context);
+    return ChangeNotifierProvider(
+        create: (_) => ProductFormProvider(productService.selectedProduct),
+        child: _ProductScreenBody(productService: productService));
+  }
+}
+
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    Key? key,
+    required this.productService,
+  }) : super(key: key);
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -58,6 +76,8 @@ class ProductScreen extends StatelessWidget {
 class _ProductForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10),
       child: Container(
@@ -70,23 +90,41 @@ class _ProductForm extends StatelessWidget {
           children: [
             SizedBox(height: 10),
             TextFormField(
+              initialValue: product.name,
+              onChanged: (value) => product.name = value,
+              validator: (value) {
+                if (value == null || value.length < 1)
+                  return 'El nombre es obligatorio';
+              },
               decoration: InputDecorations.authInputDecoration(
                   hintText: 'Nombre del Producto', labelText: 'Nombre:'),
             ),
             SizedBox(height: 30),
+
+
             TextFormField(
+              initialValue: '${product.price}',
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}'))
+              ],
+              onChanged: (value) {
+                if (double.tryParse(value) == null) {
+                  product.price = 0;
+                } else {
+                  product.price = double.parse(value);
+                }
+              },
+              
               keyboardType: TextInputType.number,
               decoration: InputDecorations.authInputDecoration(
                   hintText: '\$150', labelText: 'Precio:'),
             ),
             SizedBox(height: 30),
             SwitchListTile.adaptive(
-              value: true,
+              value: product.available,
               title: Text('Disponible'),
               activeColor: Colors.indigo,
-              onChanged: (value) {
-                // todo pendiente
-              },
+              onChanged: productForm.updateAvailability
             ),
             SizedBox(height: 30),
           ],
