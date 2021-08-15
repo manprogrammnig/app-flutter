c666cfb3d962ce9ddef06062a3b4663ab9c1408a
Franklin <201701718g@utea.edu.pe>
2021-08-15 12:33:59 -0500

    13. Manejo de imagenes nulas

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -149,7 +149,10 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
+        child:url==null
+        ?Image(image: AssetImage('assets/no-image.png'),
+        fit: BoxFit.cover,)
+         : FadeInImage(
           placeholder: AssetImage('assets/jar-loading.gif'),
           image: NetworkImage(url!),
           fit: BoxFit.cover,
