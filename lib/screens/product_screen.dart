82e5f43b30b4c622ef053c3ff03cb94ac6c713d1
Franklin <201701718g@utea.edu.pe>
2021-08-17 18:32:57 -0500

    23. Tomar fotografia o seleccionar de la galeria

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:productos_app/providers/product_form_provider.dart';
 import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
@@ -48,8 +49,17 @@ class _ProductScreenBody extends StatelessWidget {
                   top: 60,
                   right: 20,
                   child: IconButton(
-                    onPressed: () {
+                    onPressed: () async {
                       //todo:camara o galeria
+                      final picker = new ImagePicker();
+                      final PickedFile? pickedFile = await picker.getImage(
+                          source: ImageSource.camera, imageQuality: 100);
+
+                      if (pickedFile == null) {
+                        print('No selecciono nada');
+                        return;
+                      }
+                      print('Tenemos imagen ${pickedFile.path}');
                     },
                     icon: Icon(Icons.camera_alt_outlined,
                         size: 40, color: Colors.white),
@@ -67,10 +77,10 @@ class _ProductScreenBody extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.save_outlined),
-        onPressed: ()async {
+        onPressed: () async {
           //todo Guardar producto
           if (!productForm.isValidForm()) return;
-         await productService.saveOrCreateProduct(productForm.product);
+          await productService.saveOrCreateProduct(productForm.product);
         },
       ),
     );
